a2806e078f44d00bd8b4786e2d08e5d800c5f62a
Michał Wiśniewski 1 <michal.wisniewski1@microsoft.wsei.edu.pl>
2023-12-02 15:11:37 +0100

    Add check register

--- main.py
+++ main.py
@@ -4,4 +4,20 @@
 import tkinter as tk
 
 # Registers of x86 architecture 
-R8 = ["AL", "CL", "DL", "AH", "CH", "DH", "BH"]
+R8 = ["AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"]
+R16 = ["AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"]
+R32 = ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"]
+MOD = ["11", "00"]
+LABELS = []
+COUNTER = [0]
+MACHINECODES = []
+det checkRegister(register):
+    if register in R8:
+        return [8, R8.index(register)]
+    elif register in R16:
+        return [16, R16.index(register)]
+    elif register in R32:
+        return [32, R32.index(register)]
+    else:
+        return [1, "Nieprawidłowy rejestr"]
+
